31d62aa7fbda061648b5f8bce602fcb9b0886fdc
Ri-chard-Wu <glotigorgeous@gmail.com>
2024-04-10 12:35:27 +0800

    first commit

---
+++ 111022533_hw3_train.py
@@ -0,0 +1,34 @@
+import gym
+import gym_multi_car_racing
+
+# from gym.envs.classic_control import rendering
+
+env = gym.make("MultiCarRacing-v0", num_agents=1, direction='CCW',
+        use_random_direction=True, backwards_flag=True, h_ratio=0.25,
+        use_ego_color=False)
+
+obs = env.reset()
+done = False
+total_reward = 0
+
+
+
+
+
+
+while not done:
+  # The actions have to be of the format (num_agents,3)
+  # The action format for each car is as in the CarRacing-v0 environment.
+#   action = my_policy(obs)
+  action = [0, 1, 0]
+
+  # Similarly, the structure of this is the same as in CarRacing-v0 with an
+  # additional dimension for the different agents, i.e.
+  # obs is of shape (num_agents, 96, 96, 3)
+  # reward is of shape (num_agents,)
+  # done is a bool and info is not used (an empty dict).
+  obs, reward, done, info = env.step(action)
+  total_reward += reward
+#   env.render()
+
+print("individual scores:", total_reward)
---
+++ install_packages.sh
@@ -0,0 +1,22 @@
+
+pip install swig
+pip install Box2D
+pip install pygame
+pip install pyglet==1.5.27
+pip install shapely
+apt-get install python-opengl
+
+pip install setuptools==66
+cp requirements.py /usr/local/lib/python3.8/dist-packages/wheel/vendored/packaging/requirements.py
+pip install gym==0.20.0
+
+apt-get update
+apt-get install -y python-opengl
+ 
+apt install -y xvfb
+apt-get install -y libfontconfig1-dev
+
+# Run with '''xvfb-run -s "-screen 0 1400x900x24" python 111022533_hw3_train.py'''
+
+
+# apt-get update && apt-get install -y vim
