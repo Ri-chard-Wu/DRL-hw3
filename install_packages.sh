a2b63b1cab23ef50e84368c09d32b842febc76b1
Ri-chard-Wu <glotigorgeous@gmail.com>
2024-04-20 20:41:09 +0800

    add doc

--- install_packages.sh
+++ install_packages.sh
@@ -22,3 +22,14 @@ pip install pillow
 
 
 # apt-get update && apt-get install -y vim
+
+
+# -------------- HW3 環境安裝後，發生NoSuchDisplayException的error ----------------------------------------
+# 我有遇到相同問題。我後來的解決辦法是先裝下面兩個 package:
+ 
+# apt install -y xvfb
+# apt-get install -y libfontconfig1-dev
+ 
+# 然後用以下方式來執行 python 腳本:
+
+#  xvfb-run -s "-screen 0 1400x900x24" python <Student_ID>_hw3_train.py
